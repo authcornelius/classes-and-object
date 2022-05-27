1afcf563c7b814dc4acc356928dcfcb7e6e05724
oaikhienancornelius97 <corneliusedos@gmail.com>
2022-05-27 11:16:04 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing_extensions import Self
+
+
+class Student:
+
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name):
+        self.name = name
+        pass
+
+    def change_name():
+        print()
+
+    def change_name():
+        Self.name = "prter"
+        
+    def add_track():
+        print()
+
+    def get_score():
+        print ()
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
